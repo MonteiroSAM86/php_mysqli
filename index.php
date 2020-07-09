76f54e2760be3c18071126e4d9adb8166c52910a
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-10 00:19:16 +0100

    alteracoes

--- index.php
+++ index.php
@@ -15,14 +15,34 @@
         <div>
 
             <?php
-                
-                echo phpinfo();
-                /*$db_host="localhost";
+                              
+                $db_host="localhost";
                 $db_nome="condominio";
                 $db_user="root";
                 $db_password="";
 
-                $conn=mysqli_conn*/
+                $conn=mysqli_connect($db_host, $db_user, $db_password);
+                //podemos acrescentar o nome da base de dados em separado
+                mysqli_select_db($conn, $db_nome) or die ("Base de Dados não encontrada");
+
+                if (mysqli_connect_errno()) {
+                    echo "ERRO ligação Base de Dados";
+                    exit();
+                }
+                
+                mysqli_set_charset($conn, "utf8");
+
+                $consulta="SELECT data, descricao, valor FROM banco";
+
+                $resultado=mysqli_query($conn, $consulta);
+
+                while (($row = mysqli_fetch_row($resultado)) == true ) {
+                    echo $row[0] ." | ".$row[1]." | ".$row[2]."<br>";
+                }
+
+                
+
+
 
             ?>
 
