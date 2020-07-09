254e54ef39d57825f7a1984d64637f384725d3d0
Hugo Monteiro <32527057+MonteiroSAM86@users.noreply.github.com>
2020-07-09 23:32:53 +0100

    start

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso de PHP MySQL</title>
+    <link rel="stylesheet" href="_css/estilo.css">
+
+</head>
+<body>
+    <header>
+        <h1>Curso de PHP MySQL</h1>
+    </header>
+    <section>
+        <div>
+
+            <?php
+                $db_host="localhost";
+                $db_nome="condominio";
+                $db_user="root";
+                $db_password="";
+
+                $conn=mysqli_conn
+
+            ?>
+
+        </div>
+        
+    </section>
+    
+    <footer>
+        <p>&copy;Hugo Monteiro</p>
+    </footer>
+
+</body>
+</html>
